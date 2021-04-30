d480482b649e6a4c96d6b120d44ada3716d8266b
Daniel Chicot <danielchicot@digital.uc.dwp.gov.uk>
2021-04-30 10:57:48 +0100

    tweaks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/python3
 
 import argparse
 import base64
@@ -23,12 +23,12 @@ def main():
 
 
 def hbase_record(table: str, uc_id: str) -> dict:
-    checksum = binascii.crc32(uc_id.encode("ASCII"), 0).to_bytes(4, 'little')
+    checksum = binascii.crc32(uc_id.encode("ASCII"), 0).to_bytes(4, 'big')
     hbase_id: bytes = checksum + uc_id.encode("ASCII")
-    connection = happybase.Connection("hbase")
+    connection = happybase.Connection()
     connection.open()
     table = connection.table(table)
-    row = table.row(hbase_id.decode())
+    row = table.row(hbase_id)
     record = json.loads(row[b'cf:record'].decode())
     connection.close()
     return record
